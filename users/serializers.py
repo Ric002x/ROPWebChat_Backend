18ed1b776d59908fd0fb685900b330074b6f1e1a
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-13 01:24:42 -0300

    feat(users): implement Users Serializer

---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'avatar', 'name', 'email', 'last_access']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['avatar'] = f'{settings.CURRENT_URL}{instance.avatar}'
+
+        return data
