f51dc1512966197d28ff72826d349cd49164c677
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-13 01:26:36 -0300

    feat(auth): create signin and sugnup functions for user authentication

---
+++ users/auth.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.hashers import check_password, make_password
+
+from users.models import User
+
+
+class Authentication:
+    def signin(self, email: str, password: str) -> User | bool:
+        user = User.objects.filter(email=email).first()
+
+        if user and check_password(password, user.password):
+            return user
+
+        return False
+
+    def signup(self, email: str, name: str, password: str) -> User | bool:
+        if User.objects.filter(email=email).exists():
+            return False
+
+        user = User(
+            email=email,
+            name=name,
+            password=make_password(password)
+        )
+
+        return user
